c5cdb7e28cd957a1c5e8008895c216a9b4a95b4c
pasindu ranasinghe <pasinduranasinghe186@gmail.com>
2025-05-28 20:08:01 +0530

    add risk logging on successful staff login; implement final data submission in new vehicle inspection

--- lib/Screens/newInspection.dart
+++ lib/Screens/newInspection.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously, camel_case_types
 
+import 'dart:convert';
 import 'dart:io';
 import 'package:customer_portal/global_data.dart';
 import 'package:flutter/material.dart';
@@ -473,8 +474,27 @@ class _newvehicleInspecState extends State<newvehicleInspec> {
       Navigator.of(context).pop();
 
       if (failedUploads.isEmpty) {
-        _showSuccessDialog();
-        _resetState();
+        try {
+          final dataResponse = await http.post(
+            Uri.parse(
+                'http://124.43.209.68:9010/imageuploaddata/v1/saveimageuploaddata'),
+            headers: {'Content-Type': 'application/json'},
+            body: jsonEncode({
+              'nic_sfc': sfccode ?? '',
+              'pol_type': widget.policyType,
+            }),
+          );
+
+          if (dataResponse.statusCode == 200) {
+            _showSuccessDialog();
+            _resetState();
+          } else {
+            _showErrorDialog(
+                'Images uploaded but failed to submit final data.');
+          }
+        } catch (e) {
+          _showErrorDialog('Images uploaded but error submitting final data.');
+        }
       } else {
         _showErrorDialog('Failed to send images: ${failedUploads.join(", ")}');
       }
